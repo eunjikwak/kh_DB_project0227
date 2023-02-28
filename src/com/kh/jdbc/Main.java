5d4da28757b4cfd677f2e0a4a0a852529c480b15
eunjikwak <eje11@naver.com>
2023-02-28 10:55:06 +0900

    메뉴 클래스 생성

--- src/com/kh/jdbc/Main.java
+++ src/com/kh/jdbc/Main.java
@@ -12,6 +12,10 @@ public class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         CustomerDAO cDao = new CustomerDAO();
+
+        Menu menu = new Menu();
+
+
         while (true) {
             System.out.println("========== [CUSTOMER TABLE Command] ==========");
             System.out.println("메뉴를 선택 하세요 : ");
@@ -26,7 +30,7 @@ public class Main{
                     cDao.customerInsert();
                     break;
 
-                case 3 :
+                case 3 : menu.menu3();
                 case 4 :
                 case 5 :
                     System.out.println("메뉴를 종료합니다.");
---
+++ src/com/kh/jdbc/Menu.java
@@ -0,0 +1,15 @@
+package com.kh.jdbc;
+
+import java.util.Scanner;
+
+public class Menu {
+
+
+    public void menu1(){}
+    public void menu2(){}
+    public void menu3(){
+        System.out.print("메뉴 3번 메소드 호출");
+    }
+    public void menu4(){}
+
+}
